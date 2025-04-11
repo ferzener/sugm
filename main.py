5326395ff3ac2d65a5f1d9e96d209be66d724409
Ferzener <ferzener+github@proton.me>
2025-04-12 00:36:24 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# "banco de dados"
+# ler arquivo
+# escrever arquivo
+
+# diretorio de metadados
+# diretorio de usuarios
+# diretorio de grupos
+
+# criar usuario (com username nome sobrenome email e senha)
+# listar usuarios
+# deletar usuario
+# atualizar dados do usuario (nome sobrenome email)
+# autenticar usuario
+# atualizar senha de usuario
+# resetar senha de usuario
+# remover usuario
+
+# criar grupo
+# adicionar usuario a grupo
+# remover usuario de grupo
+# listar usuarios de grupo
+# remover grupo
