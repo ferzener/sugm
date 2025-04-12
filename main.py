a80626f93cae1fb9349ec87e66fb1c0ead262ae3
Pedro Mussato <pedromussato@Pedros-MacBook-Air.local>
2025-04-12 02:03:27 +0100

    creating authentication function

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+from re import I
 import filelock
 import json
 import datetime
 import hashlib
+import uuid
 
 
 # diretorio base dos dados
@@ -11,6 +13,10 @@ BASE_DIR = "/tmp/sugm"
 USERS_BASE_DIR = os.path.join(BASE_DIR, "users/")
 # diretorio de grupos
 GROUPS_FILE = os.path.join(BASE_DIR, "groups/")
+# arquivo de tokens
+TOKENS_FILE = os.path.join(BASE_DIR, "auth_tokens.json")
+# TTL para os tokens (86400s == 1 dia)
+TOKENS_TTL = 86400
 
 
 # "banco de dados"
@@ -72,7 +78,7 @@ def list_users():
     file_list = []
     for f in os.listdir(USERS_BASE_DIR):
         if not ".lock" in f and os.path.isfile(os.path.join(USERS_BASE_DIR, f)):
-            file_list.append(f[:-10])
+            file_list.append(f[:-5])
     return (len(file_list), file_list)
 
 
@@ -83,25 +89,48 @@ def delete_user(username):
 
 
 # atualizar dados do usuario (nome sobrenome email)
-def update_user_data(
-    username: str, first_name: str, last_name: str, email: str, password: str
-):
+def update_user_data(username: str, first_name: str, last_name: str, email: str):
     user_file_name = USERS_BASE_DIR + username + ".json"
 
     if not os.path.exists(user_file_name):
         return (False, "User does not exists.")
 
-    user = {
-        "first_name": first_name,
-        "last_name": last_name,
-        "email": email,
-        "password": calculate_sha256(password),
-    }
+    user = read_file(USERS_BASE_DIR + username + ".json")
+    user["first_name"] = user["first_name"] or first_name
+    user["last_name"] = user["last_name"] or last_name
+    user["email"] = user["email"] or email
     write_file(user_file_name, user)
     return (True, "User data updated with SUCCESS!")
 
 
 # autenticar usuario
+def authenticate_user(username: str, password: str):
+    user_file_name = USERS_BASE_DIR + username + ".json"
+
+    if not os.path.exists(user_file_name):
+        return (False, "User does not exists.")
+
+    if (
+        not calculate_sha256(password)
+        == read_file(USERS_BASE_DIR + username + ".json")["password"]
+    ):
+        return (False, "Incorrect password.")
+
+    token = str(uuid.uuid4())
+    current_datetime = datetime.datetime.now()
+    tokens = read_file(TOKENS_FILE)
+    if not tokens:
+        tokens = {}
+
+    tokens[token] = {
+        "username": username,
+        "created_at": f"{current_datetime.year}-{current_datetime.month}-{current_datetime.day}-{current_datetime.hour}-{current_datetime.min}-{current_datetime.second}",
+    }
+    write_file(TOKENS_FILE, tokens)
+
+    return (True, token)
+
+
 # atualizar senha de usuario
 # resetar senha de usuario
 # remover usuario
@@ -124,17 +153,18 @@ print(
     )
 )
 # list users
-print(list_users())
+# print(list_users())
 # update user data
-print(
-    update_user_data(
-        "default_username",
-        "default_first_name1",
-        "default_last_name1",
-        "default_email1",
-        "default_password1",
-    )
-)
-# delete user
-delete_user("default_username")
-print(list_users())
+# print(
+#    update_user_data(
+#        "default_username",
+#        "default_first_name1",
+#        "default_last_name1",
+#        "default_email1",
+#    )
+# )
+## delete user
+# delete_user("default_username")
+# print(list_users())
+# create token
+print(authenticate_user("default_username", "default_password"))
