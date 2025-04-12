b524c4fbd0c2daf7607e319f70a0cd36505af93c
Pedro Mussato <pedromussato@Pedros-MacBook-Air.local>
2025-04-12 01:31:42 +0100

    installing black and linting main file

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ GROUPS_FILE = os.path.join(BASE_DIR, "groups/")
 # ler arquivo
 def read_file(cache_file: str, timeout: int = -1):
     """Safetly read data to cache."""
-    with filelock.FileLock(f'{cache_file}.lock', timeout=timeout):
+    with filelock.FileLock(f"{cache_file}.lock", timeout=timeout):
         if not os.path.exists(cache_file):
             return None
         with open(cache_file) as fd:
@@ -30,15 +30,15 @@ def read_file(cache_file: str, timeout: int = -1):
 # escrever arquivo
 def write_file(cache_file: str, data: object, timeout: int = -1):
     """Safetly write data to cache."""
-    with filelock.FileLock(f'{cache_file}.lock', timeout=timeout):
-        with open(cache_file, 'w') as fd:
+    with filelock.FileLock(f"{cache_file}.lock", timeout=timeout):
+        with open(cache_file, "w") as fd:
             json.dump(data, fd)
 
 
 # deletar arquivo
 def delete_file(cache_file: str, timeout: int = -1):
     """Safely delete the cache file."""
-    with filelock.FileLock(f'{cache_file}.lock', timeout=timeout):
+    with filelock.FileLock(f"{cache_file}.lock", timeout=timeout):
         if os.path.exists(cache_file):
             os.remove(cache_file)
 
@@ -49,18 +49,19 @@ def calculate_sha256(content):
 
 
 # criar usuario (com username nome sobrenome email e senha)
-def create_user(username: str, first_name: str, last_name: str, email: str, password: str):
+def create_user(
+    username: str, first_name: str, last_name: str, email: str, password: str
+):
     user_file_name = USERS_BASE_DIR + username + ".json"
 
     if os.path.exists(user_file_name):
         return (False, "Username already in use.")
 
-
     user = {
-        "first_name":first_name,
-        "last_name":last_name,
-        "email":email,
-        "password":calculate_sha256(password)
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
+        "password": calculate_sha256(password),
     }
     write_file(user_file_name, user)
     return (True, "User created with SUCCESS!")
@@ -80,22 +81,25 @@ def delete_user(username):
     delete_file(USERS_BASE_DIR + username + ".json")
     delete_file(USERS_BASE_DIR + username + ".json.lock")
 
+
 # atualizar dados do usuario (nome sobrenome email)
-def update_user_data(username: str, first_name: str, last_name: str, email: str, password: str):
+def update_user_data(
+    username: str, first_name: str, last_name: str, email: str, password: str
+):
     user_file_name = USERS_BASE_DIR + username + ".json"
 
     if not os.path.exists(user_file_name):
         return (False, "User does not exists.")
 
-
     user = {
-        "first_name":first_name,
-        "last_name":last_name,
-        "email":email,
-        "password":calculate_sha256(password)
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
+        "password": calculate_sha256(password),
     }
     write_file(user_file_name, user)
     return (True, "User data updated with SUCCESS!")
+
 
 # autenticar usuario
 # atualizar senha de usuario
@@ -110,11 +114,27 @@ def update_user_data(username: str, first_name: str, last_name: str, email: str,
 
 # testes
 # create user
-print(create_user("default_username", "default_first_name", "default_last_name", "default_email", "default_password"))
+print(
+    create_user(
+        "default_username",
+        "default_first_name",
+        "default_last_name",
+        "default_email",
+        "default_password",
+    )
+)
 # list users
 print(list_users())
 # update user data
-print(update_user_data("default_username", "default_first_name1", "default_last_name1", "default_email1", "default_password1"))
+print(
+    update_user_data(
+        "default_username",
+        "default_first_name1",
+        "default_last_name1",
+        "default_email1",
+        "default_password1",
+    )
+)
 # delete user
 delete_user("default_username")
 print(list_users())
